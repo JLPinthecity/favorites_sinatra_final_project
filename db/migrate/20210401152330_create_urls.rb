b5968bbc14153f7f30d2154db138d83fb34330fb
github username <github email address>
2021-04-01 11:43:16 -0400

    migrate urls table

---
+++ db/migrate/20210401152330_create_urls.rb
@@ -0,0 +1,9 @@
+class CreateUrls < ActiveRecord::Migration[6.0]
+  def change
+    create_table :urls do |t|
+      t.text :link
+      t.belongs_to :user
+      t.belongs_to :favorite
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_151918) do
+ActiveRecord::Schema.define(version: 2021_04_01_152330) do
 
   create_table "favorites", force: :cascade do |t|
     t.string "category"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_04_01_151918) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "urls", force: :cascade do |t|
+    t.text "link"
+    t.integer "user_id"
+    t.integer "favorite_id"
+    t.index ["favorite_id"], name: "index_urls_on_favorite_id"
+    t.index ["user_id"], name: "index_urls_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
