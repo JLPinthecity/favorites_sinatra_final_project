1d92fd12e2cfd0a7e643f585d185ede76520742f
github username <github email address>
2021-04-01 19:27:33 -0400

    add post favorites route

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class FavoritesController < ApplicationController
 
   get '/favorites' do 
@@ -11,5 +12,20 @@ class FavoritesController < ApplicationController
         redirect to '/login'
     end
   end
+
+  post '/favorites' do
+    if logged_in? 
+      @favorite = current_user.favorites.create(:category => params[:favorite][:category], :name => params[:favorite][:name], :notes => params[:favorite][:notes])
+      @favorite.url = Url.new(params[:url])
+    
+  
+    binding.pry
+    end
+  end
+
+  #{"favorite"=>{"category"=>"My favorite cookie recipe", 
+ # "name"=>"The Best Chewy Chocolate Chip Cookies Recipe", 
+ # "notes"=>"Note to self: Brown the butter.", 
+ # "url"=>"https://tasty.co/recipe/the-best-chewy-chocolate-chip-cookies"}
 
 end
