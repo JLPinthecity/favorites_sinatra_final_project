f5bf1925b4c69dc16c76ad344bb0f48ee886330a
github username <github email address>
2021-04-01 12:54:17 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
-#create 20 users
-
-20.times do 
+#create 5 users
+5.times do 
   username = Faker::Internet.username
   email = Faker::Internet.email
   password = Faker::Internet.password 
   User.create(:name => username, :email => email, :password => password)
 end
 
-#create 40 favorites
+#create 10 favorites
 
-40.times do 
+10.times do 
   category = "favorite movie"
   name = Faker::Movie.title
   notes = Faker::Movie.quote
   Favorite.create(:category => category, :name => name, :notes => notes)
 end
 
-#create 20 urls
+#create 10 urls
 
-20.times do 
+10.times do 
   link = Faker::Internet.url 
   Url.create(:link => link)
 end
