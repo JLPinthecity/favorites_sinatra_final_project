cfbbc72051c729b0e542559bd32b5e4f7fade4af
github username <github email address>
2021-04-02 18:09:20 -0400

    dreset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-#create 5 users
-5.times do 
-  username = Faker::Internet.username
-  email = Faker::Internet.email
-  password = Faker::Internet.password 
-  User.create(:name => username, :email => email, :password => password)
-end
+# #create 5 users
+# 5.times do 
+#   username = Faker::Internet.username
+#   email = Faker::Internet.email
+#   password = Faker::Internet.password 
+#   User.create(:name => username, :email => email, :password => password)
+# end
 
-#create 10 favorites
+# #create 10 favorites
 
-10.times do 
-  category = "favorite movie"
-  name = Faker::Movie.title
-  notes = Faker::Movie.quote
-  Favorite.create(:category => category, :name => name, :notes => notes)
-end
+# 10.times do 
+#   category = "favorite movie"
+#   name = Faker::Movie.title
+#   notes = Faker::Movie.quote
+#   Favorite.create(:category => category, :name => name, :notes => notes)
+# end
 
-#create 10 urls
+# #create 10 urls
 
-10.times do 
-  link = Faker::Internet.url 
-  Url.create(:link => link)
-end
+# 10.times do 
+#   link = Faker::Internet.url 
+#   Url.create(:link => link)
+# end
 
 
 
